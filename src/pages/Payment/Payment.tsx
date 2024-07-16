129f879fbbaee4f2f2f51b5459ce5ca2914fc867
Venkatesh Saiprasath <venkatesh.tonosmith@gmail.com>
2024-07-16 16:25:58 -0700

    Updates

--- src/pages/Payment/Payment.tsx
+++
@@ -1,8 +0,0 @@
-function Payment() {
-    return (
-        <div>
-            <h1>This is the Payment page</h1>
-        </div>
-    );
-}
-export default Payment;
--- src/pages/Signup/Signup.tsx
+++
@@ -1,8 +0,0 @@
-function Signup() {
-    return (
-        <div>
-            <h1>This is the Signup page</h1>
-        </div>
-    );
-}
-export default Signup;
